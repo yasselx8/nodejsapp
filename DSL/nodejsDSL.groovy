806be51e37fe5cb67d7d0a8635c573c52c58c892
yasselx8 <153200520+yasselx8@users.noreply.github.com>
2024-09-11 10:24:05 -0400

    Create nodejsDSL.groovy

---
+++ DSL/nodejsDSL.groovy
@@ -0,0 +1,37 @@
+job('Aplicacion Node.js DSL') {
+    description('Aplicación Node JS DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('macloujulian')
+            node / gitConfigEmail('macloujulian@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        shell("npm install")
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
