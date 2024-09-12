f81082244bc04c65a5a17924b254ae2f1d8b9b95
yasselx8 <153200520+yasselx8@users.noreply.github.com>
2024-09-12 09:21:14 -0400

    Update nodejsDockerDSL.groovy
    correciones

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/yasselx8/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('yasselx8')
+            node / gitConfigEmail('yassel.leonardo@ambiente.gob.do')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('yasselx8/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
